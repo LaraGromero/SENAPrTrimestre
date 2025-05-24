7b327c8f0a92da460670244e0d2021106a80a15c
Lara Gonzalez <lauragonzalezromero83@gmail.com>
2025-05-24 14:51:25 -0500

    Trabajos hechos en clase

--- ACT_13MAY25.py
+++ ACT_13MAY25.py
@@ -21,3 +21,14 @@ T8=(N8,N8**2)
 T9=(N9,N9**2)
 T10=(N10,N10**2)
 print (T1,T2,T3,T4,T5,T6,T7,T8,T9,T10)
+L2=[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10]
+print ("---- OPERACIONES ----")
+Result= N1+N2+N3+N4+N5+N6+N7+N8+N9+N10
+print (Result)
+R= Result/10
+print (R)
+Res= Result*2
+print (Res)
+Rel= R/2
+L3= [N2**N5,N7/N3,N10+N6,N8-N1,N9*N4]
+print (L3)
