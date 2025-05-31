1d0c46d36c4cf89513e68a64597ad67a5d2c1e3f
Lara Gonzalez <lauragonzalezromero83@gmail.com>
2025-05-31 17:16:49 -0500

    Clase condicionales

---
+++ CONDICIONALCLASS.py
@@ -0,0 +1,31 @@
+E=int(input("Ingrese su edad:")) 
+if E < 18: 
+    print ("Usted es menor de edad")
+else: 
+    print ("Usted es mayor de edad")
+    
+    
+#Multiples If en un identacion.
+X=25
+if X>10:
+    print ("Por encima de diez")
+    if X>20:   #Se puede colocar otro if dentro de la identacion del primero pero jamas dos if seguidos sin que ni uno de ellos este dentro del otro. 
+        print ("y tambien pro encima de 20")
+    else:
+        print("pero no por encima de 20")
+        
+
+print ("---- EJERCICIO ----")
+A= int(input("Ingrese un numero: "))
+B= int(input("Ingrese otro numero: "))
+C= A*B
+if C<50:
+    print (C,"Es menor que 50")
+elif C<=10:
+    print (C,"Es menor o igual a 100")
+elif C>100:
+    print (C,"Es mayor a 100")
+else: 
+    print ("No cumple")
+    
+print ("---- EJERCICIO ----")
